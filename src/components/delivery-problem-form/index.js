8602d095278bcb64bc2f8b80b5377d9c251eb19c
Reldalk <Reldalk@gmail.com>
2018-11-14 14:25:24 -0500

    found lonely curly bracket

--- src/components/delivery-problem-form/index.js
+++ src/components/delivery-problem-form/index.js
@@ -32,38 +32,50 @@ function isANumber(value) {
 
 class DeliveryProblemForm extends React.Component {
   onSubmit(values) {
-    console.log(JSON.stringify(values));
-    return fetch(
-      'https://us-central1-delivery-form-api.cloudfunctions.net/api/report',
-      {
+    return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
         method: 'POST',
-        mode: 'cors',
         body: JSON.stringify(values),
-        headers: { 'Content-Type': 'application/json' },
-      }
-    )
-      .then((res) => {
-        if (!res.ok) {
-          if (
-            res.headers.has('content-type') &&
-            res.headers.get('content-type').startsWith('application/json')
-          ) {
-            return res.json().then((error) => Promise.reject(error));
-          }
-          return Promise.reject({ code: res.status, message: res.statusText });
+        headers: {
+            'Content-Type': 'application/json'
         }
-        return;
-      })
-      .catch((error) => {
-        const { reason, message, location } = error;
-        if (reason === 'ValidationError') {
-          return Promise.reject(new SubmissionError({ [location]: message }));
-        }
-        return Promise.reject(
-          new SubmissionError({ _error: 'Error submitting message' })
-        );
-      });
-  }
+    })
+        .then(res => {
+            if (!res.ok) {
+                if (
+                    res.headers.has('content-type') &&
+                    res.headers
+                        .get('content-type')
+                        .startsWith('application/json')
+                ) {
+                    // It's a nice JSON error returned by us, so decode it
+                    return res.json().then(err => Promise.reject(err));
+                }
+                // It's a less informative error returned by express
+                return Promise.reject({
+                    code: res.status,
+                    message: res.statusText
+                });
+            }
+            return;
+        })
+        .then(() => console.log('Submitted with values', values))
+        .catch(err => {
+            const {reason, message, location} = err;
+            if (reason === 'ValidationError') {
+                // Convert ValidationErrors into SubmissionErrors for Redux Form
+                return Promise.reject(
+                    new SubmissionError({
+                        [location]: message
+                    })
+                );
+            }
+            return Promise.reject(
+                new SubmissionError({
+                    _error: 'Error submitting message'
+                })
+            );
+        });
+}
 
   render() {
     return (
