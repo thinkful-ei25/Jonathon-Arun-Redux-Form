d0095b157fd6f22efb049b40cbbbbcb3cb3e7032
Reldalk <Reldalk@gmail.com>
2018-11-14 12:00:49 -0500

    added redux form

--- src/components/delivery-problem-form/index.js
+++ src/components/delivery-problem-form/index.js
@@ -1,24 +1,33 @@
 'use strict';
 import React from 'react'
+import {reduxForm, Field} from 'redux-form';
+import Input from '../input';
 
-export default class DeliveryProblemForm extends React.Component {
+
+class DeliveryProblemForm extends React.Component {
+
+
+
+
+
   render() {
     return (
       <form>
-        <label htmlFor="number">tracking number</label>
-        <input type="number" name="trackingNumber"></input>
-        <label htmlFor="issue">What is your issue?</label>
-        <select name="issue">
+        <Field type="number" name="trackingNumber" component={Input} label="tracking number"/>
+        <Field name="issue" component={Input} element="select" label="What is your issue?">
           <option value="not-delivered">My Delivery has not arrived</option>
           <option value="wrong-item">Wrong item was delivered</option>
           <option value="missing-part">Part I ordered was missing</option>
           <option value="damaged">Some of my order arrived damaged</option>
           <option value="other">Other (give details below)</option>
-        </select>
-        <label htmlFor="details">Give me more details</label>
-        <textarea name="details"></textarea>
+        </Field>
+        <Field name="details" component={Input} element="textarea" label="Give me more details"/>
         <button type="submit">Submit</button>
       </form>
     )
   }
 }
+
+export default reduxForm({
+  form: 'deliveryForm'
+})(DeliveryProblemForm)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DeliveryProblemForm from './delivery-problem-form';
+import store from './store.js'
+import {Provider} from 'react-redux'
+import DeliveryProblemForm from './components/delivery-problem-form';
 
 ReactDOM.render(
-  <DeliveryProblemForm/>, 
+  <Provider store={store}>
+    <DeliveryProblemForm/>
+  </Provider>,
   document.getElementById('root'));
+  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
